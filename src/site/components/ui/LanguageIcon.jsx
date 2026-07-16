8b912174e498691aba659532a0eeee9ecc6f43fb
Alireza <aliareya2003@gmail.com>
2026-07-16 17:37:37 +0430

    fff

--- src/site/components/ui/LanguageIcon.jsx
+++ src/site/components/ui/LanguageIcon.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Icon } from "@iconify/react";
+import useLangStore from "../../../stores/LangStore";
 // import { useLanguageStore } from "../../../store/LanguageStore";
 
-function LanguageIcon({myclass = null}) {
-  const [lang , setLang]=useState('en');
-  // const setSiteLang = useSite((state) => state.setSiteLang);
-  // const lang = useLanguageStore((state) => state.siteLang);
+function LanguageIcon({ myclass = null }) {
+  const { language, changeLanguage } = useLangStore();
+  const [lang, setLang] = useState(language);
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
   const handleLanguageChange = (code) => {
     localStorage.setItem("siteLang", code);
-    // setSiteLang(code);
+    changeLanguage(code)
     setLang(code)
     setOpen(false);
   }
-
 
   useEffect(() => {
     const handleClick = (e) => {
@@ -35,7 +34,7 @@ function LanguageIcon({myclass = null}) {
 
   return (
     <div className="relative" ref={ref}>
-      
+
       {/* Trigger */}
       <button
         onClick={() => setOpen(!open)}
@@ -48,14 +47,13 @@ function LanguageIcon({myclass = null}) {
 
       {/* Dropdown */}
       <div
-        className={`${lang === "fa" || lang === "ps" ? "right-0" :"right-0"} z-50 absolute  mt-2 w-40 border border-gray-200 bg-white shadow-lg rounded-md overflow-hidden transition-all duration-200 ${
-          open ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"
-        }`}
+        className={`${lang === "fa" || lang === "ps" ? "right-0" : "right-0"} z-50 absolute  mt-2 w-40 border border-gray-200 bg-white shadow-lg rounded-md overflow-hidden transition-all duration-200 ${open ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"
+          }`}
       >
         {languages.map((item) => (
           <button
             key={item.code}
-            onClick={()=>handleLanguageChange(item.code)}
+            onClick={() => handleLanguageChange(item.code)}
             className="w-full text-left px-4 py-2  hover:bg-gray-100 border border-gray-100"
           >
             {item.label}
--- src/site/layouts/SiteLayout.jsx
+++ src/site/layouts/SiteLayout.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Topbar from './Topbar'
 import useLangStore from '../../stores/LangStore'
 import Header from './Header';
+import { useTranslation } from 'react-i18next';
 
 function SiteLayout() {
   const {language , changeLanguage} = useLangStore();
+  const {t} = useTranslation('common');
   return (
     <div>
       <Topbar/>
       <Header/>
       {language}
+     <h1> {t('save')}</h1>
 
       <span onClick={()=>changeLanguage('fa')}>fa</span>
       <span onClick={()=>changeLanguage('en')}>en</span>
