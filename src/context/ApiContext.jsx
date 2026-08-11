efa45499951485a1edddba91a0f6739949f45d59
Alireza <aliareya2003@gmail.com>
2026-08-12 02:02:23 +0430

    fixe front bug

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -7,7 +7,7 @@ const ApiContext = createContext(null);
 
 export const ApiProvider = ({ children }) => {
   const apiurl = 'http://localhost:3000/api';
-  const production_api_url = "https://areyabazaarapi.vercel.app/api"
+  const production_api_url = "https://areyabazaarapi.vercel.app/api";
 
   return (
     <ApiContext.Provider value={{apiurl , production_api_url}}>
